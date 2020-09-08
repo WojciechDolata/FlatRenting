7dc07336c9cd01279311f7ded87e3c11ec7a9fab
WojciechDolata <33723173+WojciechDolata@users.noreply.github.com>
2020-09-08 21:59:10 +0200

    initial commit

---
+++ backend/src/main/java/agh/wd/flatrenting/FlatrentingApplication.java
@@ -0,0 +1,13 @@
+package agh.wd.flatrenting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatrentingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatrentingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/agh/wd/flatrenting/FlatrentingApplicationTests.java
@@ -0,0 +1,13 @@
+package agh.wd.flatrenting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatrentingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
