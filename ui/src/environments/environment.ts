7d712e4490a2b7bad7fe835f4f5a1cf83afa8303
WojciechDolata <wojciech.k.dolata@gmail.com>
2020-11-01 14:58:58 +0100

    heroku update environment.ts with new url

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  SERVER_URL: 'http://localhost:8080/',
+  // SERVER_URL: 'http://localhost:8080/',
+  SERVER_URL: 'https://flat-renting.herokuapp.com/',
   OFFER_PAGE_SIZE: 12,
 };
 
